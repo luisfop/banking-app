4e872e13f394ad2c7c99b709f87e7802add6ba17
luisop <luisfop77@gmail.com>
2020-09-18 15:55:19 -0300

    installed dependencies and started styling components

--- src/pages/Banking.js
+++ src/pages/Banking.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+
+import { Card, Button } from "react-bootstrap";
 
 const Banking = () => {
-    return (
-        <div>
-            <h1>Banking-App</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Banking-App</h1>
 
-export default Banking
+      <Card style={{width: '600px'}}>
+        <Card.Header>My Bank Account</Card.Header>
+        <Card.Body>
+          
+          <Button variant="primary">Deposit</Button>
+          <Button variant="danger">Withdraw </Button> 
+          <p>Your total balance is: R$:100,00 </p>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+
+export default Banking;
