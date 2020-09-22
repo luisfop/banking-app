c7130776b1e4fcc351518cfb8cfcc3f9f565edb7
luisop <luisfop77@gmail.com>
2020-09-22 15:05:33 -0300

    added back button to homepage

--- src/pages/Banking.js
+++ src/pages/Banking.js
@@ -2,15 +2,19 @@ import React from "react";
 
 import { Card, Button } from "react-bootstrap";
 
-const Banking = () => {
-  return (
-    <div>
-      <h1>Banking-App</h1>
+const Banking = (props) => {
 
+    
+    
+
+  return (
+      
+    <div>
+      <Button style={{display: 'block'}} onClick={() => props.history.push('/')}>Back</Button>
+      <h1>Banking-App</h1>
       <Card style={{width: '600px'}}>
         <Card.Header>My Bank Account</Card.Header>
         <Card.Body>
-          
           <Button variant="primary">Deposit</Button>
           <Button variant="danger">Withdraw </Button> 
           <p>Your total balance is: R$:100,00 </p>
